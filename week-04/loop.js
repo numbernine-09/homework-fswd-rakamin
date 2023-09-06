ede975e8d2a98449ef743c8a96495c9623b3c577
numbernine-09 <111489517+numbernine-09@users.noreply.github.com>
2023-09-06 21:18:14 +0700

    menambahkan beberapa code

--- week-04/loop.js
+++ week-04/loop.js
@@ -16,16 +16,50 @@ for (let i = 0; i < 5; i++) {
   console.log("for:", i);
 }
 
-// do while
+// // do while
 let index = 0;
 do {
   console.log("do while:", index);
   index++;
 } while (index < 5);
 
-// while
+// // while
 index = 0;
 while (index < 5) {
   console.log("while:", index);
   index++;
+}
+
+// *****
+// ****
+// ***
+// **
+// *
+
+let number = 5;
+for (let i = number; i >= 0; i--) {
+  let stars = " ";
+  for (let j = 0; j < i; j++) {
+    stars += "*";
+  }
+  console.log(stars);
+}
+
+/*
+ *****
+ *   *
+ *   *
+ *****
+ */
+
+for (let i = 0; i < number; i++) {
+  let stars = "";
+  for (let j = 0; j < number; j++) {
+    if (i === 0 || i === number - 1 || j === 0 || j === number - 1) {
+      stars += "*";
+    } else {
+      stars += " ";
+    }
+  }
+  console.log(stars);
 }
